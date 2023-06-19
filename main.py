c7d20861a5619c7dcc4470602699a1bcc5003043
moein805 <moein805@gmail.com>
2023-06-19 12:15:10 +0330

    commit ke 1

---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+from datetime import date, time, datetime
+import datetime
+
+import random
+
+
+total_day = 365 #total days back
+
+## make random 
+repo_link = "https://github.com/moein805/map"
+
+tl = total_day #time day
+ctr = 1
+
+now = datetime.datetime.now()
+
+f = open("commit.txt", "w")
+os.system("git config user.name")
+os.system("git config user.email")
+os.system("git init")
+
+pointer = 0
+
+while tl > 0:
+    ct = random.randint(9,10) 
+    while ct > 0:
+        f = open("commit.txt", "a+")
+        l_date = now + datetime.timedelta(days=-pointer)
+        formatdate = l_date.strftime("%Y-%m-%d")
+        f.write(f"commit ke {ctr}: {formatdate}\n")
+        f.close()
+        os.system("git add .")
+        os.system(f"git commit --date=\"{formatdate} 12:15:10\" -m \"commit ke {ctr}\"")
+        print(f"commit ke {ctr}: {formatdate}")
+        ct-=1
+        ctr+=1
+    pointer+=1
+    tl-=1
+
+os.system(f"git remote add origin {repo_link}")
+os.system("git branch -M main")
+os.system("git push -u origin main -f")
